bdfdf2dd2af92b5567385190048772769b58caa2
Samuel Mcilwain <samuelflynnmcilwain@gmail.com>
2023-11-14 16:48:23 +1100

    chore(tidy): Remove a print

--- visory/api/event/utils/geolocator.py
+++ visory/api/event/utils/geolocator.py
@@ -2,7 +2,6 @@ from geopy.geocoders import Nominatim
 
 
 def raw_location_information(address: str) -> dict[str, str] | None:
-    print('hello')
     location_information = Nominatim(user_agent="geolocator").geocode(address)
     if location_information:
         return {
